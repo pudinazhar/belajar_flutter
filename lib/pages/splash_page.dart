f31721de1fbf3c8d04535aaa25a9f1a244a321fb
najzmi.github.io <pudin@al-azhar.or.id>
2026-03-02 17:37:13 +0700

    Icon, name Apps, splashScreen
    Mengganti Icon, name Apps, dan membuat splashScreen

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import '../main.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MainPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          "assets/logo.png",
+          width: 150,
+        ),
+      ),
+    );
+  }
+}
