690b9477f41e3e8ae74390f23f5fdc18a7110c0c
najzmi.github.io <pudin@al-azhar.or.id>
2026-03-02 16:19:37 +0700

    Perubahan desain moderen
    Sudah Material 3
    BottomNav modern
    Color scheme dynamic
    AppBar clean

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Home Page 🏠",
-        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Card(
+          elevation: 3,
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Welcome to Material 3 🚀",
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ),
       ),
     );
   }
